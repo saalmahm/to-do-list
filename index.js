1e8e0c1a340ee8f9c133dc7b07d01c81eb1be2b6
salma hamdi <saalmahm51@gmail.com>
2024-10-31 09:23:59 +0100

    l'ajout de statut dans le formulaire

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const addForm = document.getElementById("task-form");
 const cancelBtn = document.getElementById("cancel");
 const toDoList = document.getElementById("to-do-list");
 const modal = document.getElementById("modal");
+const inProgressList = document.getElementById("in-progress-list");
+const doneList = document.getElementById("done-list");
 
 addTaskBtn.addEventListener("click", () => {
     modal.classList.remove("hidden"); // Affiche le modal
@@ -20,10 +22,11 @@ addForm.addEventListener("submit", (event) => {
     const title = document.getElementById("task-title").value;
     const date = document.getElementById("task-date").value;
     const priority = document.getElementById("task-priority").value;
+    const category = document.getElementById("task-category").value;
 
     // Crée un nouvel élément de tâche
     const taskElement = document.createElement("div");
-    taskElement.classList.add("p-4", "rounded", "border", "white");
+    taskElement.classList.add("p-4", "rounded", "border", "text-white");
 
     if (priority.includes("P1 (en rouge)")) {
         taskElement.classList.add("bg-red-600");
@@ -32,12 +35,16 @@ addForm.addEventListener("submit", (event) => {
     } else if (priority.includes("P3 (en vert)")) {
         taskElement.classList.add("bg-green-600");
     }
-
     taskElement.innerHTML = `<strong>${title}</strong><br><span>${date}</span>`;
 
-    toDoList.appendChild(taskElement);
-
+    // Ajoute la tâche dans la catégorie sélectionnée
+    if (category === "to-do-list") {
+        toDoList.appendChild(taskElement);
+    } else if (category === "in-progress-list") {
+        inProgressList.appendChild(taskElement);
+    } else if (category === "done-list") {
+        doneList.appendChild(taskElement);
+    }
     addForm.reset();
-    addForm.classList.add("hidden"); // Cache le formulaire après l'ajout
-    modal.classList.add("hidden"); // Cache le modal après l'ajout
+
 });
